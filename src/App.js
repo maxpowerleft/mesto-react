de56228e6a45d2f57aedfe8ead459faf3c0a88d6
maxpowerleft <lllilllilllilllilll@yandex.ru>
2021-03-17 04:19:29 +0300

    feat: create popups

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import Header from './components/Header.js';
 import Main from './components/Main.js';
 import Footer from './components/Footer.js';
+import PopupWithForm from './components/PopupWithForm.js';
+import ImagePopup from './components/ImagePopup.js';
 import './App.css';
 // import closeImage from './images/popup__close.svg';
+
 
 function App() {
   return (
@@ -11,70 +14,12 @@ function App() {
         <Header />
         <Main />
         <Footer />
+        <PopupWithForm name="profile" title="Редактировать профиль" buttonText="Сохранить" />
+        <PopupWithForm name="elements" title="Новое место" buttonText="Сохранить" />
+        <PopupWithForm name="avatar" title="Обновить аватар" buttonText="Сохранить" />
+        <PopupWithForm name="delete" title="Вы уверены?" buttonText="Да" />
+        <ImagePopup />
       </>
-
-
-  {/* <section className="popup popup_type_profile">
-  <form action="#" className="popup__form popup__profile-form" noValidate>
-    <img src={closeImage} alt="Кнопка закрытия модального окна" className="popup__close" />
-    <div className="popup__content">
-      <h3 className="popup__title">Редактировать профиль</h3>
-      <input id="user-name-input" type="text" className="popup__input popup__text popup__user-name" name="name" placeholder="Имя" required minLength={2} maxLength={40} />
-      <span id="user-name-input-error" className="popup__input-error" />
-      <input id="user-description-input" type="text" className="popup__input popup__text popup__user-description" name="about" placeholder="Описание" required minLength={2} maxLength={200} />
-      <span id="user-description-input-error" className="popup__input-error" />
-      <button type="submit" className="popup__save-button">Сохранить</button>
-    </div>
-  </form>
-</section>
-
-
-  <section className="popup popup_type_elements">
-  <form action="#" className="popup__form popup__elements-form" noValidate>
-    <img src={closeImage} alt="Кнопка закрытия модального окна" className="popup__close popup__close_element" />
-    <div className="popup__content">
-      <h3 className="popup__title">Новое место</h3>
-      <input id="card-name-input" type="text" className="popup__input popup__text popup__card-name" name="name" placeholder="Название" required minLength={1} maxLength={30} />
-      <span id="card-name-input-error" className="popup__input-error" />
-      <input id="card-src-input" type="url" className="popup__input popup__text popup__card-src" name="link" placeholder="Ссылка на фото/картинку" required />
-      <span id="card-src-input-error" className="popup__input-error" />
-      <button type="submit" className="popup__save-button popup__save-button_element">Сохранить</button>
-    </div>
-  </form>
-</section>
-
-
-  <section className="popup popup_type_fullscreen">
-  <div className="popup__fullscreen-container">
-    <img src={closeImage} alt="Кнопка закрытия модального окна" className="popup__close popup__close_element popup__close_fullscreen" />
-    <img src="#" alt="Фотография на весь экран" className="popup__fullscreen-image" />
-    <p className="popup__fullscreen-text" />
-  </div>
-</section>
-
-
-  <section className="popup popup_type_avatar">
-  <form action="#" className="popup__form popup__avatar-form" noValidate>
-    <img src={closeImage} alt="Кнопка закрытия модального окна" className="popup__close popup__close_avatar" />
-    <div className="popup__content">
-      <h3 className="popup__title">Обновить аватар</h3>
-      <input id="avatar-src-input" type="url" className="popup__input popup__text popup__avatar-src" name="link" placeholder="Ссылка на Ваш новый аватар" required />
-      <span id="avatar-src-input-error" className="popup__input-error" />
-      <button type="submit" className="popup__save-button popup__save-button_avatar">Сохранить</button>
-    </div>
-  </form>
-</section>
-
-
-  <section className="popup popup_type_delete">
-  <form action="#" className="popup__form popup__delete-form" noValidate>
-    <img src="<%=require('./images/popup__close.svg')%>" alt="Кнопка закрытия модального окна" className="popup__close popup__close_delete" />
-    <div className="popup__content">
-      <h3 className="popup__title">Вы уверены?</h3>
-      <button type="submit" className="popup__save-button popup__save-button_delete">ДА</button>
-    </div>
-  </form>
-</section> */}
 
 
   {/* <template class="template-elements">
--- src/components/Main.js
+++ src/components/Main.js
@@ -5,33 +5,48 @@ import addImage from '../images/add-image.svg';
 
 
 function Main() {
+
+const handleEditAvatarClick = () => {
+  let a = document.querySelector('.popup_type_avatar')
+  a.classList.add('popup_opened')
+}
+
+const handleEditProfileClick = () => {
+  let a = document.querySelector('.popup_type_profile')
+  a.classList.add('popup_opened')
+}
+
+const handleAddPlaceClick = () => {
+  let a = document.querySelector('.popup_type_elements')
+  a.classList.add('popup_opened')
+}
+
   return (
     <main className="content">
       <section className="profile">
         <div className="profile__container">
-          <button className="profile__edit-button-avatar">
+          <button onClick={handleEditAvatarClick} className="profile__edit-button-avatar">
             <img src={avatarLoadingGif} alt="Здесь находится ваш аватар" className="profile__avatar" />
           </button>
         </div>
         <div className="profile__user-info">
           <p className="profile__user-name">Загрузка...</p>
-          <button className="profile__edit-button">
+          <button onClick={handleEditProfileClick} className="profile__edit-button">
             <img src={editImage} alt="Кнопка изменения профиля" className="profile__edit-image" />
           </button>
           <p className="profile__user-description">Загрузка...</p>
         </div>
-        <button className="profile__add-button">
+        <button onClick={handleAddPlaceClick} className="profile__add-button">
           <img src={addImage} alt="Кнопка добавления новых записей" className="profile__add-image" />
         </button>
       </section>
       <section className="elements"></section>
-      </main>
+    </main>
+    
   )
 }
 
-// function handleEditAvatarClick() {
-//   document.querySelector('.popup_type_avatar').classList.add('popup_opened')
-// }
+
 
 // document.querySelector('.profile__edit-button-avatar').addEventListener('click', handleEditAvatarClick)
 
