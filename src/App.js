0a6e76851a71fed435de7c9d0ad72ce92c202014
maxpowerleft <lllilllilllilllilll@yandex.ru>
2021-03-17 05:49:31 +0300

    feat: реализовать открытие и закрытие попапов

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import Header from './components/Header.js';
 import Main from './components/Main.js';
 import Footer from './components/Footer.js';
@@ -8,16 +9,44 @@ import './App.css';
 
 
 function App() {
+
+  const [isEditAvatarPopupOpen, setEditAvatarPopupOpen] = React.useState(false);
+  const [isEditProfilePopupOpen, setEditProfilePopupOpen] = React.useState(false);
+  const [isAddPlacePopupOpen, setAddPlacePopupOpen] = React.useState(false);
+
+
+  const handleEditAvatarClick = () => {
+    setEditAvatarPopupOpen(true);
+  }
+
+  const handleEditProfileClick = () => {
+    setEditProfilePopupOpen(true);
+  }
+
+  const handleAddPlaceClick = () => {
+    setAddPlacePopupOpen(true);
+  }
+  
+
+  const closeAllPopups = () => {
+    setEditAvatarPopupOpen(false);
+    setEditProfilePopupOpen(false);
+    setAddPlacePopupOpen(false);
+  }
+
   return (
     <div className="page">
       <>
         <Header />
-        <Main />
+        <Main
+          onEditAvatar={handleEditAvatarClick}
+          onEditProfile={handleEditProfileClick}
+          onAddPlace={handleAddPlaceClick}/>
         <Footer />
-        <PopupWithForm name="profile" title="Редактировать профиль" buttonText="Сохранить" />
-        <PopupWithForm name="elements" title="Новое место" buttonText="Сохранить" />
-        <PopupWithForm name="avatar" title="Обновить аватар" buttonText="Сохранить" />
-        <PopupWithForm name="delete" title="Вы уверены?" buttonText="Да" />
+        <PopupWithForm onClose={closeAllPopups} isOpen={isEditProfilePopupOpen} name="profile" title="Редактировать профиль" buttonText="Сохранить" />
+        <PopupWithForm onClose={closeAllPopups} isOpen={isAddPlacePopupOpen} name="elements" title="Новое место" buttonText="Сохранить" />
+        <PopupWithForm onClose={closeAllPopups} isOpen={isEditAvatarPopupOpen} name="avatar" title="Обновить аватар" buttonText="Сохранить" />
+        <PopupWithForm onClose={closeAllPopups} name="delete" title="Вы уверены?" buttonText="Да" />
         <ImagePopup />
       </>
 
--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -4,9 +4,9 @@ import closeImage from '../images/popup__close.svg';
 function PopupWithForm(props) {
 
   return (
-    <section className={`popup popup_type_${props.name}`}>
+    <section className={`popup popup_type_${props.name} ${props.isOpen && 'popup_opened'}`}>
       <form action="#" className="popup__form popup__profile-form" noValidate>
-        <img src={closeImage} alt="Кнопка закрытия модального окна" className="popup__close" />
+        <img onClick={props.onClose} src={closeImage} alt="Кнопка закрытия модального окна" className="popup__close" />
         <div className="popup__content">
           <h3 className="popup__title">{`${props.title}`}</h3>
           {props.children}
