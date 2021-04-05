8dfe1690c22a35cbb770ae6818d3de9dde3b4e22
maxpowerleft <lllilllilllilllilll@yandex.ru>
2021-04-05 15:52:45 +0300

    feat: добавить функционал редактирования профиля

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import Main from './Main.js';
 import Footer from './Footer.js';
 import PopupWithForm from './PopupWithForm.js';
 import ImagePopup from './ImagePopup.js';
+import EditProfilePopup from './EditProfilePopup.js';
 import api from '../utils/api';
 import { CurrentUserContext } from '../contexts/CurrentUserContext.js';
 
@@ -32,6 +33,16 @@ function App() {
     setSelectedCard(card)
   }
 
+  const handleUpdateUser = (data) => {
+    api.patchUserData(data)
+      .then((res) => {
+        setCurrentUser(res)
+        closeAllPopups()
+      })
+    .catch((err) => {
+        console.error(err);
+      })
+  }
 
   const closeAllPopups = () => {
     setEditAvatarPopupOpen(false);
@@ -52,23 +63,25 @@ function App() {
 
   return (
     <CurrentUserContext.Provider value={currentUser}>
-    <div className="page">
-      <>
-        <Header />
-        <Main
-          onCardClick={handleCardClick}
-          onEditAvatar={handleEditAvatarClick}
-          onEditProfile={handleEditProfileClick}
-          onAddPlace={handleAddPlaceClick} />
-        <Footer />
-        <PopupWithForm onClose={closeAllPopups} isOpen={isEditProfilePopupOpen} name="profile" title="Редактировать профиль" buttonText="Сохранить">
+      <div className="page">
+        <>
+          <Header />
+          <Main
+            onCardClick={handleCardClick}
+            onEditAvatar={handleEditAvatarClick}
+            onEditProfile={handleEditProfileClick}
+            onAddPlace={handleAddPlaceClick} />
+          <Footer />
+          
+          <EditProfilePopup isOpen={isEditProfilePopupOpen} onClose={closeAllPopups} onUpdateUser={handleUpdateUser} />
+        {/* <PopupWithForm onClose={closeAllPopups} isOpen={isEditProfilePopupOpen} name="profile" title="Редактировать профиль" buttonText="Сохранить">
           <>
             <input id="user-name-input" type="text" className="popup__input popup__text popup__user-name" name="name" placeholder="Имя" required minLength={2} maxLength={40} />
             <span id="user-name-input-error" className="popup__input-error" />
             <input id="user-description-input" type="text" className="popup__input popup__text popup__user-description" name="about" placeholder="Описание" required minLength={2} maxLength={200} />
             <span id="user-description-input-error" className="popup__input-error" />
           </>
-        </PopupWithForm>
+        </PopupWithForm> */}
         
         <PopupWithForm onClose={closeAllPopups} isOpen={isAddPlacePopupOpen} name="elements" title="Новое место" buttonText="Сохранить">
           <>
